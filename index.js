f696031bac999c9533abe27a8217d010227363d2
Ammar Raja <ammarforuck@gmail.com>
2024-12-05 12:19:32 +0500

    Added Passenger count files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+// 1. Grab the save-el paragrah and store it in a variable called saveEl
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+
+
+
+
+function increment() {
+    count += 1
+    countEl.innerText = count
+}
+
+function save() {
+    // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
+    countString = count + " - "
+    // 3. Render the variable in the saveEl using innerText
+    saveEl.textContent += countString
+    // NB: Make sure to not delete the existing content of the paragraph
+    console.log(count)
+}
+
+save()
+
+
+
+
+
